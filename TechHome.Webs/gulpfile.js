ebfa3b4fb91d96e9d4defc2f627044a4f73e3c39
evilmax <ysq.evilmax@outlook.com>
2016-12-23 18:54:50 +1300

    1. config angularjs2

---
+++ TechHome.Webs/gulpfile.js
@@ -0,0 +1,56 @@
+﻿/// <binding Clean='clean' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+var lodash = require('lodash');
+var gulp = require('gulp');
+var del = require('del');
+
+var path = {
+    libs: {
+        angular: [
+            './node_modules/angular2/bundles/angular2.js',
+            './node_modules/angular2/bundles/router.js',
+            './node_modules/angular2/bundles/angular2-polyfills.min.js',
+            './node_modules/angular2/bundles/http.js'
+        ],
+        systemjs: [
+            './node_modules/systemjs/dist/system.src.js'           
+        ],
+        rxjs: [
+            './node_modules/rxjs/Rx.js'
+        ],
+        es6shim: [
+            './node_modules/es6-shim/es6-shim.js'
+        ]
+    }
+};
+
+gulp.task('clean', () => {
+    var cleansub = (sub) => {
+        lodash.forEach(path.libs[sub], (file) => {
+            del(file);
+        });
+    };
+    cleansub('angular');
+    cleansub('systemjs');
+    cleansub('rxjs');
+    cleansub('es6shim');
+});
+
+gulp.task('copy-libs', () => {
+    var copysub = (sub) => {
+        lodash.forEach(path.libs[sub], (file) => {
+            gulp.src(file).pipe(gulp.dest('./Scripts'));
+        });
+    };
+    copysub('angular');
+    copysub('systemjs');
+    copysub('rxjs');
+    copysub('es6shim');
+});
+
+gulp.task('default', ['copy-libs'], function () {
+    // place code for your default task here
+});
